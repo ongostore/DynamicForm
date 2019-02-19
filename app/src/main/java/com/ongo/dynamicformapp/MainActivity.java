8fcd754b33eab3999cbccdb43bb255374ed8878e
gufran khan <gkhan023>
2019-02-19 14:30:04 +0530

    added the to do

--- app/src/main/java/com/ongo/dynamicformapp/MainActivity.java
+++ app/src/main/java/com/ongo/dynamicformapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.launchDynamicFormBTN).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //ToDo add the required data
             }
         });
     }
